8b6f46321f7ef57c44295fb19e1ee4be5fc150e3
codingonHP <vishal_24_anand@hotmail.com>
2015-12-29 13:31:12 +0530

    Hello there, How are you? Glad you found us :)

---
+++ GoTango/GoTango/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GoTango.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ GoTango/GoTango/Models/MeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GoTango.Models
+{
+    // Models returned by MeController actions.
+    public class GetViewModel
+    {
+        public string Hometown { get; set; }
+    }
+}
