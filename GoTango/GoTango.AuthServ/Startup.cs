025a2fabcbc279c64b9632a1f3190440c59bd819
codingonHP <vishal_24_anand@hotmail.com>
2015-12-29 14:53:52 +0530

    Updated AuthServ with OWIN packages, and Identity Server 3

---
+++ GoTango/GoTango.AuthServ/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ADAS.GoTango.AuthServ.Startup))]
+
+namespace ADAS.GoTango.AuthServ
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
